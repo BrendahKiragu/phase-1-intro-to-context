572c60791bb35ae88cc23b90b0ea2dda92afe0d5
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-10 23:18:15 +0300

    passes test 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+//loads array elements to corresponding object properties
+//initializes empty arrays on 2 properties
+const createEmployeeRecord = (arr) => {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+};
